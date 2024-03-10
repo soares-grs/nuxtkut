c2dcc01e16c899e0038bbcb6f1f328fb47476d85
Gabriel Soares <gabrielrs.job@gmail.com>
2024-03-10 14:21:04 -0300

    chore: config head app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Nuxtkut'
+    }
+  },
   devtools: { enabled: true },
   css:["~/assets/main.css"],
   postcss: {
